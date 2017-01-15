8239d3fde7dd3672395c1c0bba7b862737f952dc
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2017-01-16 00:22:21 +0100

    Support multiple file formats

--- README.md
+++ README.md
@@ -3,11 +3,14 @@
 colortty converts terminal emulator color schemes. It currently supports the following conversions:
 
 - [mintty](https://github.com/mintty/mintty) -> [alacritty](https://github.com/jwilm/alacritty)
+- iTerm 2 -> [alacritty](https://github.com/jwilm/alacritty)
 
 ## Usage
 
 ```sh
-cat some-color.minttyrc | colortty
+cat some-color.minttyrc | colortty -i mintty
+# or
+cat some-color.itermcolors | colortty -i iterm2
 ```
 
 ## Development
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,32 @@
+extern crate getopts;
 extern crate colortty;
 
+use std::env;
 use std::io::{self, Read};
+use getopts::Options;
 use colortty::color::{ColorScheme};
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let mut opts = Options::new();
+    opts.reqopt("i", "input", "input file format", "INPUT_FORMAT");
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m)  => m,
+        Err(f) => panic!(f.to_string()),
+    };
+    let input_format = matches.opt_str("i").unwrap();
+
     let mut buffer = String::new();
     io::stdin()
         .read_to_string(&mut buffer)
         .expect("Failed to get stdin");
 
-    let scheme = ColorScheme::from_minttyrc(buffer);
+    let scheme = match input_format.as_ref() {
+        "iterm2" => ColorScheme::from_iterm2(buffer),
+        "mintty" => ColorScheme::from_minttyrc(buffer),
+        _        => panic!("Unsupported format: {}", input_format),
+    };
+
     print!("{}", scheme.to_yaml());
 }
